6f90c9e7bf458e36ba18133f64f0aaba4126d51f
ChosunOne <chosunone@protonmail.com>
2024-09-08 01:06:37 +0900

    wip: pratt parser

--- src/token.rs
+++ src/token.rs
@@ -53,3 +53,97 @@ pub enum TokenType {
     Error,
     Eof,
 }
+
+pub struct InfixBindingPower {
+    pub left_binding_power: u8,
+    pub right_binding_power: u8,
+}
+
+impl From<(u8, u8)> for InfixBindingPower {
+    fn from(value: (u8, u8)) -> Self {
+        Self {
+            left_binding_power: value.0,
+            right_binding_power: value.1,
+        }
+    }
+}
+
+pub struct PrefixBindingPower {
+    pub binding_power: u8,
+}
+
+impl PartialEq<InfixBindingPower> for PrefixBindingPower {
+    fn eq(&self, other: &InfixBindingPower) -> bool {
+        self.binding_power == other.right_binding_power
+    }
+}
+
+impl PartialOrd<InfixBindingPower> for PrefixBindingPower {
+    fn partial_cmp(&self, other: &InfixBindingPower) -> Option<std::cmp::Ordering> {
+        self.binding_power.partial_cmp(&other.right_binding_power)
+    }
+}
+
+impl From<u8> for PrefixBindingPower {
+    fn from(value: u8) -> Self {
+        Self {
+            binding_power: value,
+        }
+    }
+}
+
+pub struct PostfixBindingPower {
+    pub binding_power: u8,
+}
+
+impl PartialEq<InfixBindingPower> for PostfixBindingPower {
+    fn eq(&self, other: &InfixBindingPower) -> bool {
+        self.binding_power == other.left_binding_power
+    }
+}
+
+impl PartialOrd<InfixBindingPower> for PostfixBindingPower {
+    fn partial_cmp(&self, other: &InfixBindingPower) -> Option<std::cmp::Ordering> {
+        self.binding_power.partial_cmp(&other.left_binding_power)
+    }
+}
+
+impl From<u8> for PostfixBindingPower {
+    fn from(value: u8) -> Self {
+        Self {
+            binding_power: value,
+        }
+    }
+}
+
+impl TokenType {
+    pub fn infix_binding_power(&self) -> Option<InfixBindingPower> {
+        match self {
+            // `=` is right associative
+            Self::Equal => Some((2, 1).into()),
+            Self::And | Self::Or => Some((3, 4).into()),
+            Self::EqualEqual | Self::BangEqual => Some((5, 6).into()),
+            Self::Less | Self::LessEqual | Self::Greater | Self::GreaterEqual => {
+                Some((7, 8).into())
+            }
+            Self::Plus | Self::Minus => Some((9, 10).into()),
+            Self::Star | Self::Slash => Some((11, 12).into()),
+            // `.` and `(` are right associative
+            Self::Dot | Self::LeftParen => Some((15, 14).into()),
+            _ => None,
+        }
+    }
+
+    pub fn prefix_binding_power(&self) -> Option<PrefixBindingPower> {
+        match self {
+            Self::Bang | Self::Minus => Some(13.into()),
+            _ => None,
+        }
+    }
+
+    pub fn postfix_binding_power(&self) -> Option<PostfixBindingPower> {
+        match self {
+            _ => None,
+        }
+    }
+}
