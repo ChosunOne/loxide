094c52e13913142a308e6bb6a621655cd1bed57a
ChosunOne <chosunone@protonmail.com>
2024-09-13 04:05:38 +0900

    wip: runtime pointers

--- src/value/value_store.rs
+++ src/value/value_store.rs
@@ -1,6 +1,39 @@
-use std::{collections::BTreeMap, pin::Pin};
+use std::{
+    collections::BTreeMap,
+    ops::{Deref, DerefMut},
+    pin::Pin,
+};
 
 use crate::value::RuntimeValue;
+
+pub struct RuntimePointer<'a> {
+    value_store: &'a mut ValueStore,
+    value_ref: *const RuntimeValue,
+}
+
+impl<'a> RuntimePointer<'a> {
+    pub fn as_raw_ptr(self) -> *const RuntimeValue {
+        self.value_ref
+    }
+}
+
+impl<'a> Deref for RuntimePointer<'a> {
+    type Target = RuntimeValue;
+
+    fn deref(&self) -> &Self::Target {
+        self.value_store
+            .get(self.value_ref)
+            .expect("Failed to get value from store")
+    }
+}
+
+impl<'a> DerefMut for RuntimePointer<'a> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.value_store
+            .get_mut(self.value_ref)
+            .expect("Failed to get value from store")
+    }
+}
 
 #[derive(Default)]
 pub struct ValueStore {
@@ -9,11 +42,14 @@ pub struct ValueStore {
 }
 
 impl ValueStore {
-    pub fn insert(&mut self, value: impl Into<RuntimeValue>) -> *const RuntimeValue {
+    pub fn insert(&mut self, value: impl Into<RuntimeValue>) -> RuntimePointer {
         let pinned_object = Pin::new(Box::new(value.into()));
         let pinned_reference = &*pinned_object as *const RuntimeValue;
         self.map.insert(pinned_reference, pinned_object);
-        pinned_reference
+        RuntimePointer {
+            value_store: self,
+            value_ref: pinned_reference,
+        }
     }
 
     pub fn get(&self, key: *const RuntimeValue) -> Option<&RuntimeValue> {
@@ -40,7 +76,7 @@ mod test {
         let mut value_store = ValueStore::default();
         let value = "test string value";
         let value_ref = value_store.insert(value);
-        let retrieved_value = value_store.get(value_ref).unwrap();
+        let retrieved_value = &*value_ref;
         match retrieved_value {
             RuntimeValue::Object(o) => match &**o {
                 Object::String(s) => assert_eq!(s.chars, "test string value"),
@@ -54,9 +90,9 @@ mod test {
     fn it_gets_a_mutable_value() {
         let mut value_store = ValueStore::default();
         let value = "test string value";
-        let value_ref = value_store.insert(value);
+        let mut value_ref = value_store.insert(value);
         {
-            let mutable_value = match value_store.get_mut(value_ref).unwrap() {
+            let mutable_value = match &mut *value_ref {
                 RuntimeValue::Object(o) => match &mut **o {
                     Object::String(s) => &mut s.chars,
                     o => panic!("Unexpected object: {o}"),
@@ -66,7 +102,7 @@ mod test {
 
             *mutable_value += " mutated";
         }
-        let retrieved_value = value_store.get(value_ref).unwrap();
+        let retrieved_value = &*value_ref;
         match retrieved_value {
             RuntimeValue::Object(o) => match &**o {
                 Object::String(s) => assert_eq!(s.chars, "test string value mutated"),
@@ -80,7 +116,7 @@ mod test {
     fn it_frees_a_value() {
         let mut value_store = ValueStore::default();
         let value = "test string value";
-        let value_ref = value_store.insert(value);
+        let value_ref = value_store.insert(value).as_raw_ptr();
         value_store.free(value_ref);
         let retrieved_value = value_store.get(value_ref);
         assert!(retrieved_value.is_none())
